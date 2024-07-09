8882dc6a27129dcd0c87e27ee9e95858219e7553
Royer Fontalvo <rogerjrfontalvo@gmail.com>
2024-07-08 23:21:33 -0500

    Forma de obtener datos con js

--- consultapiscina.php
+++ consultapiscina.php
@@ -1,43 +1,19 @@
 <?php
 require 'conexion.php'; // Incluir el archivo de conexión
-
 // Consulta SQL
+
 $sql = "SELECT * FROM piscina";
 $result = $conexion->query($sql);
 
-// Preparar datos
+// Preparar datos en formato JSON
 $data = array();
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $data[] = $row;
     }
-} else {
-    $data = null;
+    var_dump($data);
+}else{
+    echo "mal";
 }
-
 $conexion->close();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="container">
-        <h1>Muestra de piscina</h1>
-        <?php if ($data): ?>
-            <?php $element = $data[0]; // Suponiendo que solo necesitas el primer registro ?>
-            <p><strong>Id: </strong><span id="idpiscina"><?php echo $element['idpiscina']; ?></span></p>
-            <p><strong>Temperatura: </strong><span id="temperatura"><?php echo $element['temp']; ?></span></p>
-            <p><strong>Salinas: </strong><span id="salinas"><?php echo $element['salina']; ?></span></p>
-            <p><strong>Hora y fecha: </strong><span id="hora"><?php echo $element['fecha']; ?></span></p>
-            <p><strong>Nombre del chip: </strong><span id="nombreChip"><?php echo $element['chip_name']; ?></span></p>
-        <?php else: ?>
-            <p>No se encontraron datos.</p>
-        <?php endif; ?>
-    </div>
-</body>
-</html>
